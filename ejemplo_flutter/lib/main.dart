8baae364cf38dd8df17ace222aa65875eab19c1b
Your Name <you@example.com>
2019-10-02 17:43:54 -0500

    Ejemplo Flutter

---
+++ ejemplo_flutter/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+        RaisedButton(
+          child: Text('Dialog'),
+          onPressed: (){
+            showDialog(        
+          context: context,
+          builder: (context) {
+          return AlertDialog(
+            title: Text('Cupertino Dialog'),
+            content: Text('This is the content of the cupertino dialog'),
+            actions: <Widget>[
+            
+            ],
+          );
+        });
+          }
+        ),
+          ],
+        )
+        
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
