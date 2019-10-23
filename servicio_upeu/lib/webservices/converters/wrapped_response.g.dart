423971029ccccf43f73112457a39947cae74abfe
davidmp <mamanipari@gmail.com>
2019-10-23 00:56:16 -0500

    Proyecto borrado

--- servicio_upeu/lib/webservices/converters/wrapped_response.g.dart
+++
@@ -1,9 +0,0 @@
-
-part of 'wrapped_response.dart';
-WrappedResponse __$WrappedResponseFromJson(Map<String, dynamic > json){
-return WrappedResponse()..data=json;
-}
-
-Map<String,dynamic> __$WrappedResponseToJson(WrappedResponse instance)=><String,dynamic>{
-  'data':instance.data,
-};
