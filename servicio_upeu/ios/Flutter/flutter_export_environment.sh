1050eefcdf158363a79dfaca7f93aa273145db1f
davidmp <mamanipari@gmail.com>
2019-10-23 06:37:11 -0500

    Se ha retrocedido a la version original del proyecto y se retrocedio de verion en barcode scan y shareth preference que no son estables en las versiones con androidx

---
+++ servicio_upeu/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\DATOS 2019\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\DAVIDMP 2019\DESARROLLO 2019\DESARROLLO GIT\Moviles2019II\servicio_upeu"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\DATOS 2019\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
